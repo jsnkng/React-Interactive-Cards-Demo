b2aa9614c19be6e2c7c2eb1281c465196bf87d96
Jason King <jking@cab408.com>
2019-11-03 18:41:37 -0500

    Modularize code
    Break App into three files.

---
+++ src/ClassCard.js
@@ -0,0 +1,245 @@
+import React,{ useState } from 'react';
+import styled from 'styled-components';
+import ChangeImage from './ChangeImage';
+import ReactCardFlip from 'react-card-flip';
+
+const ClassCard = ({content,deleteClass}) => {
+    const [featureImage, setFeatureImage] = useState(content.featureImage || 'https://via.placeholder.com/500/000000/ffffff/?text=Ceci+ne+pas+une+repas+délicieux ')
+    const [title, setTitle] = useState(content.title)
+    const [instructor, setInstructor] = useState(content.instructor)
+    const [description, setDescription] = useState(content.description)
+    const [duration, setDuration] = useState(content.duration)
+    const [classType, setClassType] = useState(content.classType)
+    const [isFlipped, setIsFlipped] = useState(false)
+  
+    const toggleFlip = (e) => {
+      setIsFlipped(!isFlipped)
+    }
+    const handleChangeTitle = (e) => { 
+      const value = e.target.value
+      setTitle(value)
+    }
+    const handleChangeInstructor = (e) => { 
+      const value = e.target.value
+      setInstructor(value)
+    }
+    const handleChangeDescription = (e) => { 
+      const value = e.target.value
+      setDescription(value)
+    }
+    const handleChangeDuration = (e) => { 
+      const value = e.target.value
+      setDuration(value)
+    }
+    const handleChangeClassType = (e) => { 
+      const value = e.target.value
+      setClassType(value)
+    }
+    const handleFormSubmit = (e) => {
+      setTitle(title)
+      setInstructor(instructor)
+      setDescription(description)
+      setDuration(duration)
+      setClassType(classType)
+      setIsFlipped(false)
+      e.preventDefault();
+    }
+  
+    return (
+      <ClassCardWrapper>
+        <ReactCardFlip isFlipped={isFlipped} flipDirection="horizontal">
+          
+          <ClassCardDisplay key="front" onClick={toggleFlip}>
+            <div style={{ backgroundImage: 'url(' + featureImage + ')', 
+                  backgroundSize: 'cover', 
+                  backgroundPosition: 'center center',
+                  backgroundRepeat: 'no-repeat',
+                  width: '100%',
+                  height: '200px',
+                  borderRadius: '2px 2px 0 0'
+            }}></div>
+            <h2>{title}</h2>
+            <h3>{instructor}</h3>
+            <p className="classDescription">{description}</p>
+            <div>
+              <span className={classType === 'live' ? 'classType live' : 'classType onDemand'}>{classType}</span>
+              <span className="classDuration">{duration} min</span>
+            </div>
+            <div></div>
+          </ClassCardDisplay>
+  
+          <ClassCardForm key="back">
+            <ChangeImage featureImage={featureImage} setFeatureImage={setFeatureImage} />
+            <label>Title: </label>
+            <input 
+              type="text"
+              name="title"
+              value={title}
+              onChange={handleChangeTitle}
+              />
+            <br />
+            <label>Instructor:</label>
+            <input
+              type="text"
+              name="instructor"
+              value={instructor}
+              onChange={handleChangeInstructor}
+              />
+            <br />
+            <label>Description:</label>
+            <textarea
+              rows="6"
+              name="description"
+              value={description}
+              onChange={handleChangeDescription}
+              />
+            <br />
+            <label>Duration:</label>
+            <input
+              type="number"
+              name="duration"
+              value={duration}
+              onChange={handleChangeDuration}
+              />
+            <label className="form__radioLabel">On-Demand: 
+              <input
+                type="radio"
+                value="on-demand"
+                onChange={handleChangeClassType}
+                checked={classType === 'on-demand'}
+              />
+            </label>
+            <label>Live: 
+              <input
+                type="radio"
+                value="live"
+                onChange={handleChangeClassType}
+                checked={classType === 'live'}
+              />
+            </label>
+            <div className="form__buttons">
+              <button onClick={handleFormSubmit}>Save</button>
+              <button onClick={(e) => deleteClass(e, content.id)}>Delete</button>
+            </div>
+          </ClassCardForm>
+  
+        </ReactCardFlip>
+      </ClassCardWrapper>
+    )
+  }
+
+const ClassCardWrapper = styled.div`
+  width: 350px;
+  margin: 5px 5px;
+`
+const ClassCardDisplay = styled.div`
+  height: 470px;
+  background: #ffffff;
+  border-radius: 2px;
+  border: solid 1px #cccccc;
+  cursor: pointer;
+  box-shadow: 3px 3px 3px 0px rgba(0,0,0,.05);
+
+  & h2 {
+    display: inline-block;
+    font-weight: 600;
+    font-size: 1.5em;
+    font-weight: 700;
+    padding: 10px;
+    margin-top: -50px;
+    max-width: 80%;
+    background: rgba(255,255,255,1);
+  }
+  & h3 {
+    font-weight: 500;
+    font-size: 1.125em;
+    padding: 0px 10px;
+    margin-top: 0px;
+  }
+  & div {
+    clear: both;
+  }
+  & .classDescription {
+    font-weight: 400;
+    font-size: .75em;
+    padding: 0 20px;
+    height: 120px;
+    overflow: hidden;
+  }
+  & .classType {
+    float: left;
+    font-weight: 700;
+    text-transform: uppercase;
+    color: #ffffff;
+    padding: 2px 10px;
+  }
+  & .classType.live {
+    background: #000000;
+  }
+  & .classType.onDemand {
+    background: #ffffff;
+    border: 1px solid #cccccc;
+    border-left: 0px;
+    color: #000000;
+  }
+  & .classDuration {
+    width: 30px;
+    height: 27px;
+    float: right;
+    border-radius: 32px;
+    font-weight:500;
+    font-size: .75em;
+    line-height: 1;
+    margin: 0 5px 0 0;
+  }    
+`
+const ClassCardForm = styled.div`
+  height: 470px;  
+  background: #ffffff;
+  border-radius: 2px;
+  border: solid 1px #cccccc;
+  cursor: pointer;
+  box-shadow: 3px 3px 3px 0px rgba(0,0,0,.05);
+
+  & .container__changeImage button {
+    position: fixed;
+    width: 150px;
+    top: 150px;
+    left: 50%;
+    margin-left: -75px;
+  }
+
+  & label {
+    display: inline-block;
+    vertical-align: top;
+    padding: 5px;
+    font-weight: 500;
+    font-size: .80em;
+    text-align: right;
+    width: 75px;
+  }
+  & label.form__radioLabel {
+    width: 125px;
+  }
+  & textarea  {
+    width: 180px;
+    font-size: .80em;
+    border: 1px solid #cccccc;
+    padding: 2px;
+  }
+  & input  {
+    width: 180px;
+    font-size: .80em;
+    border: 1px solid #cccccc;
+    padding: 2px;
+  } 
+  & input[type="radio"]  {
+    display: inline-block;
+    width: 30px;
+    border: 1px solid #cccccc;
+  }
+  & .form__buttons {
+    padding: 10px;
+  }
+`
+export default ClassCard
