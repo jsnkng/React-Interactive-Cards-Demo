a0eb7815481d3b5bdc1400c7199c9b339678e611
Jason King <jking@cab408.com>
2019-11-04 12:21:01 -0500

    Updates

---
+++ precache-manifest.bfb4685e006a1783dc837115b08c6acd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec1005c841c592aa12c3c45ed281d277",
+    "url": "/React-Interactive-Cards-Demo/index.html"
+  },
+  {
+    "revision": "dadd4a7bf0638fd54f35",
+    "url": "/React-Interactive-Cards-Demo/static/css/main.7daae8f7.chunk.css"
+  },
+  {
+    "revision": "33737f4b6dc107c6c2a3",
+    "url": "/React-Interactive-Cards-Demo/static/js/2.dcbdd075.chunk.js"
+  },
+  {
+    "revision": "dadd4a7bf0638fd54f35",
+    "url": "/React-Interactive-Cards-Demo/static/js/main.3ade70c1.chunk.js"
+  },
+  {
+    "revision": "8e9e86310ae7a4bef906",
+    "url": "/React-Interactive-Cards-Demo/static/js/runtime-main.375d035d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bdadaad65dcab706d7c71dce9aa8e362.js"
+  "/React-Interactive-Cards-Demo/precache-manifest.bfb4685e006a1783dc837115b08c6acd.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/React-Interactive-Cards-Demo/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
